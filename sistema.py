8b2f899b038da97f14c9e448befe2d89cffa74d4
eduardofonseca2 <eduardo.mcf@puccampinas.edu.br>
2026-05-02 12:02:31 -0300

    tirei o for e coloquei o while e break

--- sistema.py
+++ sistema.py
@@ -4,23 +4,33 @@ menor = 10**9
 verdes = 0
 total = 0
 vermelhas_consecutivas = 0
-for i in range(n):
-    if vermelhas_consecutivas < 3: 
-        leitura = float(input(f"Leitura {i+1}: "))
-        if leitura > 150:
-            leitura = leitura * 1.08
-        else:
-            leitura = leitura * 0.96
-        soma += leitura
-        total += 1
-        if leitura < menor:
-            menor = leitura
-        if 120 <= leitura <= 180:
-            verdes += 1
-            vermelhas_consecutivas = 0
-        elif leitura < 250:
-            vermelhas_consecutivas = 0
-        else:
-            vermelhas_consecutivas += 1
+travado = False
+i = 0
+
+while i < n:
+    leitura = float(input(f"Leitura {i+1}: "))
+    
+    if leitura > 150:
+        leitura = leitura * 1.08
     else:
-        print(f"leitura {i+1} ignorada (sistema travado)")
+        leitura = leitura * 0.96
+    
+    soma += leitura
+    total += 1
+    
+    if leitura < menor:
+        menor = leitura
+    
+    if 120 <= leitura <= 180:
+        verdes += 1
+        vermelhas_consecutivas = 0
+    elif leitura < 250:
+        vermelhas_consecutivas = 0
+    else:
+        vermelhas_consecutivas += 1
+        if vermelhas_consecutivas >= 2:
+            print("sistema travado: dois picos críticos consecutivos!")
+            travado = True
+            break
+    
+    i += 1
